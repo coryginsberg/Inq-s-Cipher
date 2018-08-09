9e0297642aa4d83f060dcd928a565e6f4aaec5a1
Cory Ginsberg <cory.ginsberg@perficient.com>
2018-08-09 13:16:49 -0500

    Realized I was doing this wrong and doing much more than the cipher requires. This is a dead-end because of that.

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -313,3 +313,26 @@ module.exports = {
     hints: false,
   },
 };
+
+var config = {
+  entry: ["./app.tsx"],
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  }
+};
+
+module.exports = config;
--- src/Cipher.js
+++ src/Cipher.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+const ALPHABET = [...Array(26).keys()].map(i => String.fromCharCode(i + 'A'.charCodeAt(0))); // Creates an array of the alphabet
 
 export default class CipherForm extends Component {
 
@@ -10,8 +11,13 @@ export default class CipherForm extends Component {
       listLetter: 'A',
       listNumber: 0,
       textbox: '',
+      tempAlphabet: [],
     }
+
+    console.log(ALPHABET);
   }
+
+  
 
   onSubmit = (event) => {
     this.setState({[event.target.name]: event.target.value}); // Universal setting of this.state.*
@@ -21,22 +27,53 @@ export default class CipherForm extends Component {
     let textToEncrypt = this.state.textbox;
     console.log(textToEncrypt);
 
-    let alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
-    let newAlphabet = alphabet;
+    const newAlphabet = ALPHABET.slice();
+
+    // const newAlphabet = ALPHABET.map(letter => {
+    //   return null;
+    // });
+
+    console.log()
 
     let holdLetters = [];
 
     for (let i = 0; i < this.state.listNumber; i++) {
-      holdLetters.push(alphabet[i]);
+      holdLetters.push(ALPHABET[i]);
     }
     for (let i = 0; i < this.state.listNumber; i++) {
-      newAlphabet.shift(alphabet[i]);
+      newAlphabet.shift(ALPHABET[i]);
     }
-    newAlphabet.push(holdLetters);
+    newAlphabet.push(holdLetters).slice();
+
+    this.computeCipher(textToEncrypt, newAlphabet);
   }
 
   handleChange = (event) => {
     this.setState({[event.target.name]: event.target.value});
+  }
+
+  // For this we need to get the position (index) of each letter in the original alphabet,
+  // and use that to get the new letter from the new alphabet.
+  computeCipher(textToEncrypt, newAlphabet) {
+    let txtUnencrypted = textToEncrypt.toUpperCase();
+
+    let positions = []; // Array to hold the position of the letters in the old alphabet.
+
+    for (let txt of txtUnencrypted) {
+      positions.push(newAlphabet.indexOf(txt));
+      console.log("txt: " + txt);
+      console.log("IndexOf: " + newAlphabet.indexOf(txt));
+    }
+
+    let txtEncrypted = [];
+    for (let position of positions) {
+      txtEncrypted.push(newAlphabet[position]);
+    }
+    console.log("Encrypted Text: " + txtEncrypted);
+    console.log("Alphabet: " + ALPHABET);
+    console.log("New Alphabet: " + newAlphabet);
+    console.log("Positions: " + positions);
+
   }
 
   render() {
@@ -59,20 +96,24 @@ export default class CipherForm extends Component {
     return (
       <div>
         <form>
-          <div className="input-field">
-            <textarea id="Text" className="materialize-textarea" type="text" name="textbox" value={this.state.value} onChange={this.handleChange}></textarea>
+          <div className="Textbox-Wrapper">
             <label className="txtarea-label" htmlFor="Text">Enter your text here</label>
+            <textarea id="Text" className="materialize-textarea" type="text" name="textbox" value={this.state.value} onChange={this.handleChange}></textarea>
           </div>
           <br/>
-          <label htmlFor="Letter">Select a letter</label>
-          <select id="Letter" name="listLetter" onChange={this.handleChange}>
-            {listLetters}
-          </select>
+          <div className="LetterDropbown-Wrapper">
+            <label htmlFor="Letter">Select a letter</label>
+            <select id="Letter" name="listLetter" onChange={this.handleChange}>
+              {listLetters}
+            </select>
+          </div>
           <br/>
-          <label htmlFor="Number">Select a number</label>
-          <select id="Number" name="listNumber" onChange={this.handleChange}>
-            {listNumbers}
-          </select>
+          <div className="NumberDropdown-Wrapper">
+            <label htmlFor="Number">Select a number</label>
+            <select id="Number" name="listNumber" onChange={this.handleChange}>
+              {listNumbers}
+            </select>
+          </div>
           <br />
           <button className="small-btn mdc-button mdc-button--raised" onClick={this.onSubmit}>Encrypt</button>
         </form>
