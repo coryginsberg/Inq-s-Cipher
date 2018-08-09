72980324e0f6b3d3424351e2184bcc4ffb888793
Cory Ginsberg <cory.ginsberg@perficient.com>
2018-08-09 15:38:33 -0500

    Got the first offset to work and print correctly

--- src/Cipher.js
+++ src/Cipher.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+const ALPHABET = [...Array(26).keys()].map(i => String.fromCharCode(i + "A".charCodeAt(0)));
 
 export default class CipherForm extends Component {
 
@@ -10,6 +11,8 @@ export default class CipherForm extends Component {
       listLetter: 'A',
       listNumber: 0,
       textbox: '',
+      cipherValue: '',
+      cipherComputed: false,
     }
   }
 
@@ -18,66 +21,91 @@ export default class CipherForm extends Component {
 
     event.preventDefault(); // Self Explanitory IMO
 
-    let textToEncrypt = this.state.textbox;
-    console.log(textToEncrypt);
+    const textToEncrypt = this.state.textbox.toUpperCase().split("");
+    const cipherValue = parseInt(this.state.listNumber, 10);
+    console.log(cipherValue);
 
-    let alphabet = [...Array(26).keys()].map(i => String.fromCharCode(i + 97));
-    let newAlphabet = alphabet;
+    let newText = textToEncrypt.map(txtChar => {
+      if (ALPHABET.includes(txtChar)) {
+        return parseInt(ALPHABET.indexOf(txtChar) + 2, 10) + cipherValue + " "
+      } else {
+        return txtChar + " ";
+      }
+    });
 
-    let holdLetters = [];
+    console.log(newText);
 
-    for (let i = 0; i < this.state.listNumber; i++) {
-      holdLetters.push(alphabet[i]);
-    }
-    for (let i = 0; i < this.state.listNumber; i++) {
-      newAlphabet.shift(alphabet[i]);
-    }
-    newAlphabet.push(holdLetters);
+    this.setState({
+      cipherValue: newText,
+      cipherComputed: true,
+    });
   }
 
   handleChange = (event) => {
     this.setState({[event.target.name]: event.target.value});
   }
 
-  render() {
-
-    let letters = [...Array(26).keys()].map(i => String.fromCharCode(i + 97)); // Trust me, this makes the alphabet into an Array.
-
-    const listLetters = letters.map((letter) =>
-      <option value={letter}>{letter}</option>
-    );
-
-    const numbers = [];
-    for (let i = 1; i <= 26; i++) {
+  calculateOffset(start, end) {
+    const numbers = []
+    for (let i = start; i <= end; i++) {
       numbers.push(i);
     }
+    return numbers;
+  }
 
-    const listNumbers = numbers.map((number) =>
+  render() {
+
+    const numbersFirst = this.calculateOffset(1, 26);
+    const listFirstNumbers = numbersFirst.map((number) =>
+      <option value={number}>{number}</option>
+    );
+
+    const numbersSecond = this.calculateOffset(27, 52);
+    const listSecondNumbers = numbersSecond.map((number) =>
+      <option value={number}>{number}</option>
+    );
+
+    const numbersThird = this.calculateOffset(53, 78);
+    const listThirdNumbers = numbersThird.map((number) =>
+      <option value={number}>{number}</option>
+    );
+
+    const numbersFourth = this.calculateOffset(79, 99);
+    const listFourthNumbers = numbersFourth.map((number) =>
       <option value={number}>{number}</option>
     );
 
     return (
       <div>
         <form>
-          <div className="input-field">
-            <textarea id="Text" className="materialize-textarea" type="text" name="textbox" value={this.state.value} onChange={this.handleChange}></textarea>
+          <div className="txtArea-wrapper">
             <label className="txtarea-label" htmlFor="Text">Enter your text here</label>
+            <br/>
+            <textarea id="Text" className="materialize-textarea" type="text" name="textbox" value={this.state.value} onChange={this.handleChange}></textarea>
           </div>
           <br/>
-          <label htmlFor="Letter">Select a letter</label>
-          <select id="Letter" name="listLetter" onChange={this.handleChange}>
-            {listLetters}
-          </select>
+          <div className="numSelect-wrapper">
+            <label htmlFor="Number">Select an offset</label>
+            <div className="numSelect-dropdowns">
+              <select id="Number" name="listNumber" onChange={this.handleChange}>
+                {listFirstNumbers}
+              </select>
+              <select id="Number" name="listNumber" onChange={this.handleChange}>
+                {listSecondNumbers}
+              </select>
+              <select id="Number" name="listNumber" onChange={this.handleChange}>
+                {listThirdNumbers}
+              </select>
+              <select id="Number" name="listNumber" onChange={this.handleChange}>
+                {listFourthNumbers}
+              </select>
+            </div>
+          </div>
           <br/>
-          <label htmlFor="Number">Select a number</label>
-          <select id="Number" name="listNumber" onChange={this.handleChange}>
-            {listNumbers}
-          </select>
-          <br />
           <button className="small-btn mdc-button mdc-button--raised" onClick={this.onSubmit}>Encrypt</button>
         </form>
 
-        {this.state.letterSubmitted ? (
+        {this.state.cipherComputed ? (
           <h1 className="Large-letters ">{this.state.cipherValue}</h1>) : null}
       </div>
     );
